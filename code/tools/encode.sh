34c91e07871b228b8b11a25e0747929b6274cca1
Matthieu Dalstein <github@dalmat.net>
2018-11-17 00:03:19 +0100

    New codecs for encode.sh

--- code/tools/encode.sh
+++ code/tools/encode.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 bitrate=1024
-defaultcrf=22
+declare -A defaultcrf
 crf=
 preset="-preset slow"
 startt=""
@@ -12,12 +12,16 @@ deinterlace=""
 directory=""
 geometry=""
 acodec="opus"
-aquality="low"
+aquality="mid"
 vcodec="vp9"
 options=""
 ext="_rs"
+passes=("")
 
-
+defaultcrf[x264]=23
+defaultcrf[x265]=23
+defaultcrf[vp9]=31
+defaultcrf[x265]=23
 
 function usage
 {
@@ -25,7 +29,7 @@ cat << EOF
 $0 [options] <file(s) to encode>
 Options:
 	-h  Display the usage
-	-q <video quality> (default ${defaultcrf})>
+	-q <video quality> (default ${defaultcrf[x265]})>
 	-c : Rotate the video 90° clockwise
 	-p : Rotate the video 90° anti-clockwise (positive sense)
 	-s <start time> : Start the encoded video at the given time (format for 5 minutes is 300, or 05:00)
@@ -34,10 +38,11 @@ Options:
 	-i : Deinterlace the video
 	-g <XxY> : Resize the video (e.g 800x600)
 	-a <audio codec> : Specify the audio codec (e.g : opus, vorbis, aac, copy)
-	-A <audio codec> : Specify the audio quality (e.g low/high) (default: low). Note: the values depend on the codec
-	-v <video codec> : Specify the video codec (e.g : x264, x265, copy)
+	-A <audio quality> : Specify the audio quality (e.g low/mid/high) (default: low). Note: the values depend on the codec
+	-v <video codec> : Specify the video codec (e.g : x264, x265, copy, hevc_vaapi, none)
 	-x <extension> : Add an extension suffix to the output filename (default : $ext if the output extension is the same as the input file)
 EOF
+exit 0
 }
 
 while getopts "hq:cps:e:d:ig:a:A:v:x:" opt; do
@@ -58,16 +63,17 @@ while getopts "hq:cps:e:d:ig:a:A:v:x:" opt; do
 		A) aquality=$OPTARG ;;
 		v) vcodec=$OPTARG ;;
 		x) ext=$OPTARG ;;
-		h) usage; exit 0 ;;
+		h) usage ;;
 	esac
 done
 
-[ -z $crf ] && crf=$defaultcrf
 
 shift $(($OPTIND - 1))
 
 if [[ $acodec == "aac" ]]; then
         if [[ $aquality == "low" ]]; then
+            aquality=80k
+        elif [[ $aquality == "mid" ]]; then
             aquality=96k
         elif [[ $aquality == "high" ]]; then
             aquality=128k
@@ -78,6 +84,8 @@ elif [[ $acodec == "copy" ]]; then
 elif [[ $acodec == "opus" ]]; then
         if [[ $aquality == "low" ]]; then
             aquality=80k
+        elif [[ $aquality == "mid" ]]; then
+            aquality=96k
         elif [[ $aquality == "high" ]]; then
             aquality=128k
         fi
@@ -92,18 +100,30 @@ elif [[ $acodec == "vorbis" ]]; then
 fi
 
 
-
+hwaccel=""
 if [[ $vcodec == "copy" ]]; then
 	video="-c:v copy"
-elif [[ $vcodec == "x264" ]]; then
-	video="-c:v libx264 -crf $crf"
-elif [[ $vcodec == "x265" ]]; then
-	video="-c:v libx265 -crf $crf"
-elif [[ $vcodec == "vp9" ]]; then
-	preset=""
-	video="-c:v libvpx-vp9 -g 100 -threads 4 -tile-columns 6 -frame-parallel 0  -speed 2 -b:v 4M -crf $crf"
-#	video="-c:v libvpx-vp9 -threads 4 -tile-columns 6  -b:v 3M -crf $crf"
+elif [[ $vcodec == "none" ]]; then
+	video="-vn"
+else
+	[ -z $crf ] && crf=${defaultcrf[$vcodec]}
+	
+	if [[ $vcodec == "x264" ]]; then
+		video="-c:v libx264 -crf $crf"
+	elif [[ $vcodec == "x265" ]]; then
+		video="-c:v libx265 -crf $crf"
+	elif [[ $vcodec == "hevc_vaapi" ]]; then
+		video="-vaapi_device /dev/dri/renderD128 -c:v hevc_vaapi -vf format=nv12,hwupload -qp $crf"
+		hwaccel="-hwaccel vaapi"
+		preset=""
+	elif [[ $vcodec == "vp9" ]]; then
+		preset=""
+		video="-c:v libvpx-vp9 -g 200 -threads 4 -tile-columns 2 -quality good -speed 1 -b:v 0 -crf $crf"
+	#	video="-c:v libvpx-vp9 -threads 4 -tile-columns 6  -b:v 4M -crf $crf"
+		passes=("-pass 1" "-pass 2")
+	fi
 fi
+
 
 if [[ $acodec == "aac" ]]; then
     container=mp4
@@ -119,6 +139,10 @@ if [[ $container == "mkv" ]]; then
 elif [[ $container == "mp4" ]]; then
 	options="$options -movflags +faststart"
 fi
+
+for file in "$@"; do
+	if ! [ -f "$file" ]; then echo "File $file does not exist"; exit 1; fi
+done
 
 for file in "$@"; do
 
@@ -140,12 +164,15 @@ fi
 # https://sites.google.com/a/webmproject.org/wiki/ffmpeg/vp9-encoding-guide
 
 STARTTIME=$(date +%s)
+for pass in "${passes[@]}"; do
 set -x
-ffmpeg -y -i "$file" $geometry $startt $stopt $deinterlace $rotate $audio $video $preset $options "${newfile}"
+ffmpeg -y $hwaccel -i "$file" $geometry $startt $stopt $deinterlace $rotate $audio $video $preset $options $pass "${newfile}" < /dev/null
 set +x
+done
 touch -r "$file" "${newfile}"
 ELAPSEDTIMED=$(($(date +%s) - STARTTIME))
 
 echo -e "${newfile} encoded in \033[01;32m$ELAPSEDTIMED s\033[00m"
 
 done
+
